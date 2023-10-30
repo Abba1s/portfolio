f7356854565cb63dfff93f30bfb8807663da0314
Muhammad Ababs <abbiezia717@gmail.com>
2023-10-30 16:10:58 +0500

    commit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import "./index.css";
 import App from "./App";
 import "aos/dist/aos.css";
 import "remixicon/fonts/remixicon.css";
-import { Provider } from "react-redux";
-import store from "./redux/store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+  
       <App />
-    </Provider>
+   
   </React.StrictMode>
 );
 
